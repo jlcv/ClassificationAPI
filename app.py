66a0b661baae589d9d1bfecd82b2eaa6b2e5935e
jlcv <jlcv@github.com>
2017-05-11 20:27:27 -0500

    added x-api-key requirement in headers, added testing categories

--- app.py
+++ app.py
@@ -8,30 +8,47 @@ from email_predict import post_prediction
 app = Flask(__name__)
 
 base_api = "/classification_api/v1"
+api_key = "d8ccc900-7c92-4722-be58-5b747184dcb7"
+
+def check_api_key(obtained_api_key):
+    if obtained_api_key == api_key:
+        return True
+    else:
+        return False
+    
 
 @app.route(base_api + '/predict', methods=['POST'])
 def get_tasks():
-    try:
-        data = request.json
-        return jsonify({'success': True, 'prediction': json.loads(post_prediction(email_body=data['email_body'], categories=data['categories']))})
-    except Exception, e:
-        return jsonify({'error': str(e)}), 400
+    if check_api_key(obtained_api_key=request.headers.get('X-Api-Key')):
+        try:
+            data = request.json
+            return jsonify({'success': True, 'prediction': json.loads(post_prediction(email_body=data['email_body'], categories=data['categories']))})
+        except Exception, e:
+            return jsonify({'error': str(e)}), 400
+    else:
+        return jsonify({'error': 'Unrecognized Api Key'}), 400
 
 @app.route(base_api + '/new/email', methods=['POST'])
 def post_email():
-    try:
-        data = request.json
-        return jsonify({'success': post_new_email(email_body=data['email_body'], category=data['category'])})
-    except Exception, e:
-        return jsonify({'error': str(e)}), 400
+    if check_api_key(obtained_api_key=request.headers.get('X-Api-Key')):
+        try:
+            data = request.json
+            return jsonify({'success': post_new_email(email_body=data['email_body'], category=data['category'])})
+        except Exception, e:
+            return jsonify({'error': str(e)}), 400
+    else:
+        return jsonify({'error': 'Unrecognized Api Key'}), 400
 
 @app.route(base_api + '/testing', methods=['POST'])
 def get_test_results():
-    try:
-        data = request.json
-        return jsonify({'success': True, 'result': json.loads(get_training_results(categories=data['categories']))})
-    except Exception, e:
-        return jsonify({'error': str(e)}), 400
+    if check_api_key(obtained_api_key=request.headers.get('X-Api-Key')):
+        try:
+            data = request.json
+            return jsonify({'success': True, 'result': json.loads(get_training_results(categories=data['categories']))})
+        except Exception, e:
+            return jsonify({'error': str(e)}), 400
+    else:
+        return jsonify({'error': 'Unrecognized Api Key'}), 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
